dac2816f04e950752b8f837c7c163098c6a156e5
김혜림 <144879107+kimhyerims@users.noreply.github.com>
2024-11-07 23:47:02 +0900

    Revert "[5주차 Spring-Basic 스터디] 김혜림"

--- Hyerim/hello-spring/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ Hyerim/hello-spring/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,21 +1,14 @@
 package hello.hello_spring;
 
 
-import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
     @Bean
     public MemberService memberService(){
@@ -24,12 +17,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource) { //다형성을 활용. 인터페이스를 두고 구현체를 바꿔끼는 것이 가능
-            @Override
-            public void clearStore() {
-
-            }
-        };
+        return new MemoryMemberRepository();
     } // 스프링 빈에 등록
 } // 등록된 멤버 리포지토리를 멤버 서비스에 넣어줌
